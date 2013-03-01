e71446cac8a34504a87e3c416b1c10e0aed19b21
Jason Fiddle <jfiddle@ycp.edu>
2013-03-01 11:37:26 -0500

    Updates to UI

--- GamingWebUi/src/edu/ycp/cs320/gamingwebsite/client/MemView.java
+++ GamingWebUi/src/edu/ycp/cs320/gamingwebsite/client/MemView.java
@@ -5,7 +5,8 @@ import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.NumberLabel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 
 public class MemView extends Composite {
 	public MemView() {
@@ -15,12 +16,19 @@ public class MemView extends Composite {
 		
 		InlineLabel lblDisplayScore = new InlineLabel("Score:");
 		layoutPanel.add(lblDisplayScore);
-		layoutPanel.setWidgetLeftWidth(lblDisplayScore, 293.0, Unit.PX, 38.0, Unit.PX);
+		layoutPanel.setWidgetLeftWidth(lblDisplayScore, 316.0, Unit.PX, 38.0, Unit.PX);
 		layoutPanel.setWidgetTopHeight(lblDisplayScore, 0.0, Unit.PX, 26.0, Unit.PX);
 		
-		NumberLabel<Integer> intlblScore = new NumberLabel<Integer>();
-		layoutPanel.add(intlblScore);
-		layoutPanel.setWidgetLeftWidth(intlblScore, 337.0, Unit.PX, 113.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(intlblScore, 0.0, Unit.PX, 26.0, Unit.PX);
+		Label lblScore = new Label("0");
+		lblScore.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		layoutPanel.add(lblScore);
+		layoutPanel.setWidgetLeftWidth(lblScore, 360.0, Unit.PX, 90.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(lblScore, 0.0, Unit.PX, 26.0, Unit.PX);
+		
+		Button btnReplay = new Button("New button");
+		btnReplay.setText("Replay");
+		layoutPanel.add(btnReplay);
+		layoutPanel.setWidgetLeftWidth(btnReplay, 0.0, Unit.PX, 90.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(btnReplay, 260.0, Unit.PX, 40.0, Unit.PX);
 	}
 }
