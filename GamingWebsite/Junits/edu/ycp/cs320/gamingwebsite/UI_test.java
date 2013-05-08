3cbc877e2675031998db30708a347e04a2f45e9d
Samantha Hamor <shamor1@ycp.edu>
2013-05-08 15:49:47 -0400

    Player class Junits, complete coverage

--- GamingWebsite/Junits/edu/ycp/cs320/gamingwebsite/UI_test.java
+++ GamingWebsite/Junits/edu/ycp/cs320/gamingwebsite/UI_test.java
@@ -45,35 +45,35 @@ public class UI_test extends TestCase {
 	}
 	
 	
-//	@Test
-//	public void testgetcontact(){
-//		assertEquals(false, player1.getcontact());
-//		
-//		
-//	}
+	@Test
+	public void testgetcontact(){
+		assertEquals(false, player1.getcontact());
+		
+		
+	}
 	
-//	@Test
-//	public void testcollision(){
-//		//first if
-//		player1.collision(486, 50, 0, 0);
-//		assertEquals(486.0, player1.getX());
-//		assertEquals(50.0, player1.getY());	
-//		//second if
-//		player1.collision(26, 316, 0, 0);
-//		assertEquals(26.0, player1.getX());
-//		assertEquals(316.0, player1.getY());
-//		// second if's nested if
-//		player1.collision(730, 314, 0, 0);
-//		assertEquals(true, player1.getcontact());
-//		// second if's nested else
-//		player1.collision(600, 314, 0, 0);
-//		assertEquals(false, player1.getcontact());
-//		//final else
-//		player1.collision(816, 313, 1, 1);
-//		assertEquals(815.0, player1.getX());
-//		assertEquals(312.0, player1.getY());
-//	}
-//	
+	@Test
+	public void testcollision(){
+		//first if
+		player1.collision(486, 50, 0, 0);
+		assertEquals(486.0, player1.getX());
+		assertEquals(50.0, player1.getY());	
+		//second if
+		player1.collision(26, 316, 0, 0);
+		assertEquals(26.0, player1.getX());
+		assertEquals(316.0, player1.getY());
+		// second if's nested if
+		player1.collision(730, 314, 0, 0);
+		assertEquals(true, player1.getcontact());
+		// second if's nested else
+		player1.collision(600, 314, 0, 0);
+		assertEquals(false, player1.getcontact());
+		//final else
+		player1.collision(816, 313, 1, 1);
+		assertEquals(815.0, player1.getX());
+		assertEquals(312.0, player1.getY());
+	}
+	
 	
 	@Test
 
